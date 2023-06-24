60b4fc7422b2ea835415adab00e5815999c56120
Hildaboo <67108708+Hildaboo@users.noreply.github.com>
2023-06-24 03:26:54 -0700

    Snazzy!

---
+++ C#/Aero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace AeroConsole
+{
+    class Aero
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern System.UInt32 GetWindowLong(IntPtr hWnd, int nIndex);
+
+        private const int GWL_EXSTYLE   = -20;
+        private const int WS_EX_LAYERED = 0x80000;
+        private const int LWA_ALPHA     = 0x2;
+
+        /// <summary>
+        /// percentage of opacity, 0% - 255%
+        /// </summary>
+        /// <param name="percent"></param>
+        public static void MakeTransparent(byte percent)
+        {
+            IntPtr Handle = Process.GetCurrentProcess().MainWindowHandle;
+            int newDwLong = ((int)GetWindowLong(Handle, GWL_EXSTYLE)) ^ WS_EX_LAYERED;
+            
+            SetWindowLong(Handle, GWL_EXSTYLE, newDwLong);
+            SetLayeredWindowAttributes(Handle, 0, percent, LWA_ALPHA);
+        }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+
+namespace AeroConsole
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Aero.MakeTransparent(191);
+            Console.Title = "This is my world just believe me!";
+            
+            LockBitLog("Ready");
+            Thread.Sleep(1000);
+            LockBitLog("Steady");
+            Thread.Sleep(1000);
+            LockBitLog("GO!");
+
+            Console.WriteLine("Press any key to continute...");
+            Console.ReadKey();
+        }
+
+        private static void LockBitLog(string line)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(string.Format("[{0:D2}:{1:D2}:{2:D2}] ", DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second));
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(line);
+            
+            Console.ResetColor();
+        }
+    }
+}
